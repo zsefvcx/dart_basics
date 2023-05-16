b460be99bb1bbfaa3b1c89fc1dfc87a85613e26a
zsefvcx <zsefvcx@list.ru>
2023-05-16 12:00:31 +0300

    add task 7 completed - 30%

--- lib/dart_basics.dart
+++ lib/dart_basics.dart
@@ -8,5 +8,6 @@ export 'src/task_03_number_search_in_string.dart';
 export 'src/task_04_search_word_count_in_list.dart';
 export 'src/task_05_one_two_three_to_int.dart';
 export 'src/task_06_class_point.dart';
+export 'src/task_07_custom_root_n_exponent.dart';
 
 // TODO: Export any libraries intended for clients of this package.
