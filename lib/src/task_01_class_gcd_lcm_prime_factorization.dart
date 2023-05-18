32adc3109381f1803b53a436d8c5c5b1d859a145
zsefvcx <zsefvcx@list.ru>
2023-05-18 10:49:54 +0300

    task 1 various code changes

--- lib/src/task_01_class_gcd_lcm_prime_factorization.dart
+++ lib/src/task_01_class_gcd_lcm_prime_factorization.dart
@@ -1,35 +1,34 @@
-
 ///1.  Реализуйте методы вычисления НОД и НОК целых чисел. Реализуйте метод,
 ///который разбивает число - на простые множители и возвращает их.
 
 class DelimetersCalculator {
-
   final int _value;
 
-  DelimetersCalculator({required int value}) : assert(value>0) ,_value = value;
+  DelimetersCalculator({required int value})
+      : assert(value > 0),
+        _value = value;
 
   ///Наибольший общий делитель (англ. greatest common divisor)
-  int gcd(int x){
-    int min = _value>x?x:_value;
-    int max = _value>x?_value:x;
-    int search(int max, int min){
-      return min > 0 ? search(min, max%=min) : max;
-    }
+  int gcd(int x) {
+    if (x <= 0) throw Exception('Value in gcd must be greater than zero');
+    int min = _value > x ? x : _value;
+    int max = _value > x ? _value : x;
+    int search(int max, int min) => min > 0 ? search(min, max %= min) : max;
     return search(min, max);
   }
 
   ///Наименьшее общее кратное (англ. least common multiple)
-  int lcm(int x) {
-     return (_value ~/ gcd(x)) * x;
-  }
+  int lcm(int x) => x <= 0
+      ? throw Exception('Value in lcm must be greater than zero')
+      : (_value ~/ gcd(x)) * x;
 
   ///Факториза́ция натурального числа (англ. integer factorization)
-  List<int> fin(){
+  List<int> fin() {
     List<int> result = [];
     int j = 2;
     int buffer = _value;
-    while(j*j <= buffer){
-      if(buffer%j==0){
+    while (j * j <= buffer) {
+      if (buffer % j == 0) {
         result.add(j);
         buffer ~/= j;
         j = 2;
@@ -40,7 +39,6 @@ class DelimetersCalculator {
     result.add(buffer);
     return result;
   }
-
 
   @override
   String toString() {
